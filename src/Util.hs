0c30037e1cf3f457b51218646021e55ddfa3cbde
Reed Oei <oei.reed@gmail.com>
2019-03-11 15:19:27 -0500

    Added addition for root 2 automaton to k2-automata

--- src/Util.hs
+++ src/Util.hs
@@ -1,6 +1,20 @@
 module Util where
 
+import Data.List
+
 concats :: [[a]] -> [[a]]
 concats [] = [[]]
 concats (xs:xss) = [ x:rest | x <- xs, rest <- concats xss ]
+
+borders :: Eq a => [a] -> [[a]]
+borders xs = filter (not . null) $ filter (\b -> b `isSuffixOf` xs) $ takeWhile (\b -> 2 * length b <= length xs) $ inits xs
+
+bordered :: Eq a => [a] -> Bool
+bordered = not . null . borders
+
+unbordered :: Eq a => [a] -> Bool
+unbordered = not . bordered
+
+substrings :: [a] -> [[a]]
+substrings = filter (not . null) . concatMap inits . tails
 
