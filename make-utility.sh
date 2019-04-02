4b369a9c4a5aa18468438c35f06620ed940afd3c
Reed Oei <oei.reed@gmail.com>
2019-04-02 13:41:00 +0000

    Fix issue with times_1

--- make-utility.sh
+++ make-utility.sh
@@ -64,7 +64,9 @@ binary "$WALNUT_AUTOMATA_PATH/successor_${max_dig}.txt"
     echo "def times_1_${max_dig} \"?lsd_base_${max_dig} \$recog_${max_dig}(a,b) & b = c\":"
 
     for i in $(seq 1 "$max_pow"); do
-        times "$i"
+        if [[ "$i" -gt "1" ]]; then
+            times "$i"
+        fi
 
         pow "$i"
     done
